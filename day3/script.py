5bc39289e4f327af42d7c1ae057444fe60a05f76
Paul Farver <paulfarver@gmail.com>
2020-12-03 10:03:30 +0100

    Solve day 3 part 2
    Signed-off-by: Paul Farver <paulfarver@gmail.com>

--- day3/script.py
+++ day3/script.py
@@ -17,3 +17,34 @@ for l in F:
     print()
 
 print(trees)
+
+F = open("input.txt", "r")
+
+slopes = [(1,1),(3,1),(5,1),(7,1),(1,2)]
+result = [0    ,0    ,0    ,0    ,0    ]
+
+def checkSlope(slope, x, y):
+    return y%slope[1] == 0 and (y/slope[1]*slope[0])%31 == x
+
+y = 0
+for l in F:
+    for x in range(31):
+        # if l[x] == "#":
+        hit = False
+        for s in range(len(slopes)):
+            if checkSlope(slopes[s], x, y):
+                if l[x] == "#":
+                    result[s] += 1
+                hit = True
+        if hit:
+            if l[x] == "#":
+                print("X", end="")
+            else:
+                print("O", end="")
+        else:
+            print(l[x], end="")
+    print()
+    y += 1
+print(result)
+print(result[0]*result[1]*result[2]*result[3]*result[4])
+
