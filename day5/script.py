01ea29466eb1afe31779cbfa19ee9d26f4fd6143
Paul Farver <paulfarver@gmail.com>
2020-12-05 12:39:16 +0100

    Day 5 part 1
    Signed-off-by: Paul Farver <paulfarver@gmail.com>

---
+++ day5/script.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+F = open("input")
+
+def searchRow(s):
+    rows = np.array(range(128))
+    for c in s:
+        if c == "F":
+            rows = np.array_split(rows, 2)[0]
+        if c == "B":
+            rows = np.array_split(rows, 2)[1]
+        if c != "F" and c != "B":
+            raise Exception("Noo")
+    return rows[0]
+
+def searchColumn(s):
+    columns = np.array(range(8))
+    for c in s:
+        if c == "L":
+            columns = np.array_split(columns, 2)[0]
+        if c == "R":
+            columns = np.array_split(columns, 2)[1]
+    return columns[0]
+
+ids = [ searchRow(l[:7]) * 8 + searchColumn(l[7:]) for l in F ]
+print(ids)
+print(max(ids))
+# print(sorted(ids))
