90e5209f7160ef2c93504cb7811fd7fe32248134
Paul Farver <paulfarver@gmail.com>
2020-12-06 11:32:30 +0100

    day 6 part 2
    Signed-off-by: Paul Farver <paulfarver@gmail.com>

--- day6/script.py
+++ day6/script.py
@@ -8,3 +8,13 @@ for l in re.sub(r'\n(\S)', r'\1', F.read()).splitlines():
     arr = np.array(list(l))
     s += len(np.unique(arr))
 print(s)
+
+F = open("input", "r")
+s = 0
+for l in re.sub(r'\n(\S)', r' \1', F.read()).splitlines():
+    groups = [list(h) for h in l.split(" ")[1:]]
+    intersection = groups[0]
+    for i in range(len(groups)):
+        intersection = np.intersect1d(intersection, groups[i])
+    s += len(intersection)
+print(s)
