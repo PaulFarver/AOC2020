f362d421953aa38dbac2e70f8a930a5bcae0f22b
Paul Farver <paulfarver@gmail.com>
2020-12-08 17:42:24 +0100

    Day 8 part 2 solved with brute force
    Signed-off-by: Paul Farver <paulfarver@gmail.com>

--- day8/script.py
+++ day8/script.py
@@ -2,28 +2,43 @@
 
 F = open("input", "r")
 
-acc = 0
-ptr = 0
-
-def accumulate(value):
-    global acc
-    acc += int(value)
-    return 1
+def flip(inst):
+    if inst[0] == "nop":
+        return ("jmp", inst[1])
+    if inst[0] == "jmp":
+        return ("nop", inst[1])
+    if inst[0] == "acc":
+        return ("acc", inst[1])
+    raise Exception("Unexpected instruction: ", inst)
 
 operations = {
-    'acc': accumulate,
-    'nop': lambda v: 1,
-    'jmp': lambda v: int(v),
+    'acc': lambda p, a, v: (p+1, a+v),
+    'nop': lambda p, a, v: (p+1, a),
+    'jmp': lambda p, a, v: (p+v, a),
 }
 
-instructions = [(s.split(" ")[0], s.split(" ")[1]) for s in F]
+instructions = [(s.split(" ")[0], int(s.split(" ")[1])) for s in F]
 
-visited = [False]*len(instructions)
-while True:
+def call(set, ptr, acc):
+    return operations[set[0]](ptr, acc, set[1])
+
+def detect_infinite(instructions, ptr, acc, visited):
+    if ptr >= len(visited):
+        return False, ptr, acc, visited
     if visited[ptr]:
-        break
-    else:
-        visited[ptr] = True
-    ptr = ptr + operations[instructions[ptr][0]](instructions[ptr][1])
+        return True, ptr, acc, visited
+    visited[ptr] = True
+    ptr, acc = call(instructions[ptr], ptr, acc)
+    return detect_infinite(instructions, ptr, acc, visited)
 
-print(acc)
+yay, ptr, acc, _ = detect_infinite(instructions, 0, 0, [False]*len(instructions))
+print("Part 1: ", acc)
+
+for i in range(len(instructions)):
+    instructions[i] = flip(instructions[i])
+    yay, ptr, acc, _ = detect_infinite(instructions, 0, 0, [False]*len(instructions))
+    if yay:
+        instructions[i] = flip(instructions[i])
+    else:
+        print("Part 2: ", acc)
+        break
