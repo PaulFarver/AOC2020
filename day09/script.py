243722aeb7aa219a12c62a9f093c0789eb6741c9
Paul Farver <paulfarver@gmail.com>
2020-12-09 20:49:16 +0100

    Day 09 part 2
    Signed-off-by: Paul Farver <paulfarver@gmail.com>

--- day09/script.py
+++ day09/script.py
@@ -12,6 +12,30 @@ def hasPair(s, numbers):
     return False
 
 
+part1 = 0
+part1n = 0
 
 for i in range(25, len(n)):
-    print(n[i], hasPair(n[i], n[:i]))
+    if not hasPair(n[i], n[:i]):
+        part1n = i
+        part1 = n[i]
+        print(n[i], i, False)
+        break
+
+
+def findSet(target, numbers):
+    i = 0
+    j = 1
+    while True:
+        s = sum(numbers[i:j])
+        if s > target:
+            i += 1
+        if s < target:
+            j += 1
+        if s == target:
+            return i, j
+
+i, j = findSet(part1, n[:part1n])
+print(n[i:j])
+print(sum(n[i:j]))
+print(min(n[i:j])+max(n[i:j]))
